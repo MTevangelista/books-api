87533b248dec2aec90d7ccea096a3af2845779da
Matheus Evangelista <matheusevangelistadev@gmail.com>
2020-09-18 20:15:06 -0300

    refactor: refactor standard route

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,17 @@ import bookController from '@controllers/bookController';
 
 const routes = Router();
 
-routes.get('/', (req, res) => res.status(200).json({ message: 'Hello World' }));
+routes.get('/', (req, res) => res.status(200).json({
+  repository: 'https://github.com/MTevangelista/books-api',
+  routes: {
+    getAll: 'https://books-backend-api.herokuapp.com/api/books',
+    getBySlug: 'https://books-backend-api.herokuapp.com/api/books/:slug',
+    post: 'https://books-backend-api.herokuapp.com/api/books',
+    put: 'https://books-backend-api.herokuapp.com/api/books/api/books/:_id',
+    delete: 'https://books-backend-api.herokuapp.com/api/books/api/books/:_id',
+  },
+
+}));
 routes.get('/api/books', bookController.getAll);
 routes.get('/api/books/:slug', bookController.getBySlug);
 routes.post('/api/books', bookController.post);
